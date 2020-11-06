8cb100f4a66313973a108e607709ec1a9daa34d1
Ilya Soloveychik <dj.active.17@gmail.com>
2020-11-06 20:38:10 +0200

    pages design ok

--- admin.php
+++ admin.php
@@ -75,6 +75,7 @@
                         <th scope="col">Email</th>
                         <th scope="col">Task text</th>
                         <th scope="col">Status</th>
+                        <th scope="col">Actions</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -83,18 +84,33 @@
                         <td>Otto</td>
                         <td>@twitter</td>
                         <td>Completed</td>
+                        <td>
+                            <a href="/edit.php?id=1" class="btn btn-primary mb-2">Change text</a>
+                            <a href="/change_status.php?to=0&id=1" class="btn btn-warning mb-2">Change status</a>
+                            <a href="/delete.php?id=1" class="btn btn-danger mb-2">Delete status</a>
+                        </td>
                     </tr>
                     <tr>
                         <td>Jacob</td>
                         <td>Thornton</td>
                         <td>@twitter</td>
-                        <td>Not completed</td>
+                        <td>Completed</td>
+                        <td>
+                        <a href="/edit.php?id=1" class="btn btn-primary mb-2">Change text</a>
+                            <a href="/change_status.php?to=0&id=1" class="btn btn-warning mb-2">Change status</a>
+                            <a href="/delete.php?id=1" class="btn btn-danger mb-2">Delete status</a>
+                        </td>
                     </tr>
                     <tr>
                         <td>@twitter</td>
                         <td>@twitter</td>
                         <td>@twitter</td>
-                        <td>@twitter</td>
+                        <td>Completed</td>
+                        <td>
+                            <a href="/edit.php?id=1" class="btn btn-primary mb-2">Change text</a>
+                            <a href="/change_status.php?to=0&id=1" class="btn btn-warning mb-2">Change status</a>
+                            <a href="/delete.php?id=1" class="btn btn-danger mb-2">Delete status</a>
+                        </td>
                     </tr>
                 </tbody>
                 </table>
--- config.php
+++ config.php
@@ -1,10 +1,11 @@
 <?php
 
-// mysql:host=xxx;port=xxx;dbname=xxx
     $host = 'localhost';
     $port = 3307;
     $dbname = 'beejee';
     $username = 'root';
     $password = '';
+
+    $mysqli = new mysqli($host, $username, $password, $dbname, $port) or die('Error');
 
 ?>
